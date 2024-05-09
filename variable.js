8774fb402a45fbb4b462dcc3f237c3dd326aa260
wali55 <walisantunu@gmail.com>
2024-05-09 22:29:48 +0600

    js string slice

--- variable.js
+++ variable.js
@@ -4,4 +4,8 @@ const mobile = {
     price: 16000
 };
 
+const text = "sky, sea, forest";
+
 document.getElementById("new").innerHTML = `Mobile brand name ${mobile.brand}, price ${mobile["price"]}, color ${mobile.color}.`;
+
+document.getElementById("another").innerHTML = text.slice(5, 8);
