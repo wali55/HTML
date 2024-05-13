b721663c532ff36e2cad72459c07f51729c9eeee
wali55 <walisantunu@gmail.com>
2024-05-13 13:25:16 +0600

    array speard

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,3 +1,4 @@
+/*
 if (true) {
     let a = 10;
     const b = 20;
@@ -10,4 +11,11 @@ if (true) {
 
 const arr = [45, 4, 9, 16, 25];
 const newArr = arr.filter((value, index, array) => value > 10);
+console.log(newArr);
+*/
+
+const arr1 = ["sun", "moon"];
+const arr2 = ["sky", "earth"];
+
+const newArr = [...arr1, ...arr2];
 console.log(newArr);
