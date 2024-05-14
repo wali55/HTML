664afb1b0d3cebd2b6aa01580e1085f93bb3b3a6
wali55 <walisantunu@gmail.com>
2024-05-14 14:44:30 +0600

    ternary operator

--- variable.js
+++ variable.js
@@ -9,3 +9,9 @@ const text = "sky, sea, forest";
 document.getElementById("new").innerHTML = `Mobile brand name ${mobile.brand}, price ${mobile["price"]}, color ${mobile.color}.`;
 
 document.getElementById("another").innerHTML = text.slice(5, 8);
+
+function newFunction() {
+    let canVote;
+    const age = Number(document.getElementById("age").value);
+    document.getElementById("another1").innerHTML = age >= 18 ? "can vote" : "can not vote";
+}
