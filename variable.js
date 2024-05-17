d396a1a8d301c04f9e7a1be8f27a2cfa72489cb5
wali55 <walisantunu@gmail.com>
2024-05-17 11:13:28 +0600

    error handling

--- variable.js
+++ variable.js
@@ -15,3 +15,27 @@ function newFunction() {
     const age = Number(document.getElementById("age").value);
     document.getElementById("another1").innerHTML = age >= 18 ? "can vote" : "can not vote";
 }
+
+function myFunction() {
+    let heading = document.getElementById("test");
+    heading.innerHTML = "Hello ";
+    let myInput = document.getElementById("demo").value;
+    try {
+        if (myInput.trim() === "") {
+            throw "The field is empty";
+        }
+        if (isNaN(myInput)) {
+            throw "Not a number";
+        }
+        myInput = Number(myInput);
+        if (myInput < 5) {
+            throw "Too low";
+        } 
+        if (myInput > 10) {
+            throw "Too high"
+        }
+    }
+    catch(err) {
+        heading.innerHTML += err;
+    }
+}
