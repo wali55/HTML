c0beb8178ddf201d795e87f3469c51f67faa29b8
wali55 <walisantunu@gmail.com>
2024-05-18 20:05:46 +0600

    function

--- variable.js
+++ variable.js
@@ -6,9 +6,9 @@ const mobile = {
 
 const text = "sky, sea, forest";
 
-document.getElementById("new").innerHTML = `Mobile brand name ${mobile.brand}, price ${mobile["price"]}, color ${mobile.color}.`;
+// document.getElementById("new").innerHTML = `Mobile brand name ${mobile.brand}, price ${mobile["price"]}, color ${mobile.color}.`;
 
-document.getElementById("another").innerHTML = text.slice(5, 8);
+// document.getElementById("another").innerHTML = text.slice(5, 8);
 
 function newFunction() {
     let canVote;
@@ -38,4 +38,12 @@ function myFunction() {
     catch(err) {
         heading.innerHTML += err;
     }
+    finally {
+        document.getElementById("demo").value = "";
+     }
 }
+
+function newFunction(a, b) {
+    return a * b;
+}
+console.log(newFunction(10, 5));
